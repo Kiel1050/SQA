183ea0b476ff4834e58bf608e87a0949019c37f5
kiel <41059489+Kiel1050@users.noreply.github.com>
2019-03-12 10:51:26 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets\bootstrap-4.3.1-dist\css\bootstrap.css">
+  <script src="assets\bootstrap-4.3.1-dist\js\jquery-3.3.1.js"></script>
+  <script src="assets\bootstrap-4.3.1-dist\js\bootstrap.js"></script>
+</head>
+<body>
+
+<?php
+include "controller/index.php";
+?>
+
+</body>
+</html>
